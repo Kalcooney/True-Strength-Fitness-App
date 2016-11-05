e71bbeb166c20691e9fbb500bc3e3b8c0d8c5711
Kaleb Cooper <kalebcooper@gmail.com>
2016-11-05 15:40:23 +1300

    Make all screens fullscreen

--- app/src/main/java/com/example/kaleb/truestrength/exerciseList.java
+++ app/src/main/java/com/example/kaleb/truestrength/exerciseList.java
@@ -2,8 +2,11 @@ package com.example.kaleb.truestrength;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -18,6 +21,12 @@ public class exerciseList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Make the activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Hides the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_exercise_list);
 
         dbHandler = new MyDBHandler(this, null, null, 1);
--- app/src/main/java/com/example/kaleb/truestrength/firstList.java
+++ app/src/main/java/com/example/kaleb/truestrength/firstList.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -24,6 +27,12 @@ public class firstList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Make the activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Hides the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_first_list);
         dbHandler = new MyDBHandler(this, null, null, 1);
         populateListView();
